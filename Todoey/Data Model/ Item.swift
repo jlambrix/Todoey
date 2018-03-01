71314486eb222f963c89d2c38db5f6f73a68a60e
Josh Lambrix Cell <jlambrix@msn.com>
2018-02-28 21:49:07 -0500

    covert to MVC file pattern

---
+++ Todoey/Data Model/ Item.swift
@@ -0,0 +1,16 @@
+//
+//   Item.swift
+//  Todoey
+//
+//  Created by Josh Lambrix on 2/28/18.
+//  Copyright © 2018 Josh Lambrix. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title:String = ""
+    var done:Bool = false
+    
+}
